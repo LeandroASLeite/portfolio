c828261290254800cbd2961f2271e7f359c7f839
Leandro Leite <leandroleite.ll25@proton.me>
2025-04-04 20:07:10 -0300

    fix all

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  output:'export',
-  distDir:'dist',
-  images:{
-      unoptimized: true,
-  }
+  basePath: isProd ? '/portfolio' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
